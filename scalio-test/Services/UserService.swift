65124fc0a5a3934b7df5ad70fd0b5b63e0faed72
Saqib <saqibomer.cs@gmail.com>
2022-05-12 19:06:31 +0500

    - Implemented get users apis

--- scalio-test/Services/UserService.swift
+++ scalio-test/Services/UserService.swift
@@ -7,21 +7,42 @@
 
 import Foundation
 import RxSwift
-
+import Moya
 
 protocol UserServiceProtocol {
-//    func getUsers(searchString: String) -> Observable<UsersResponse>
+    func getUsers(searchString: String) -> Observable<UsersResponse>
     
 }
 
 final class UserService: UserServiceProtocol{
     
     
-//    func getUsers(searchString: String) -> Observable<UsersResponse> {
-//        return Observable.create { observer -> Disposable in
-//            
-//        }
-//    }
+    func getUsers(searchString: String) -> Observable<UsersResponse> {
+        
+        return Observable.create { observer -> Disposable in
+            let provider = MoyaProvider<GithubService>()
+            provider.request(.getUsers(login: searchString)) { result in
+                switch result {
+                case let .success(moyaResponse):
+                    let data = moyaResponse.data
+                    let statusCode = moyaResponse.statusCode
+                    print(statusCode)
+                    do {
+                        let userResponse = try JSONDecoder().decode(UsersResponse.self, from: data)
+                        observer.onNext(userResponse)
+                    } catch {
+                        observer.onError(error)
+                    }
+                case let .failure(error):
+                    observer.onError(error)
+                }
+            }
+            
+            return Disposables.create {
+                
+            }
+        }
+    }
     
         
 }
