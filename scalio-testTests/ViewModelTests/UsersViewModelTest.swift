eb5b1df22c689e2f6cf26669b0f2e39170d9ecb3
Saqib <saqibomer.cs@gmail.com>
2022-05-13 13:32:26 +0500

    - Updated unit test for UsersViewModel

--- scalio-testTests/ViewModelTests/UsersViewModelTest.swift
+++ scalio-testTests/ViewModelTests/UsersViewModelTest.swift
@@ -18,4 +18,18 @@ class UsersViewModelTest: XCTestCase {
         XCTAssertNotNil(viewModel, "The view model should not be nil.")
     }
 
+    func testBinders() {
+            let scheduler = TestScheduler(initialClock: 0)
+            let source = scheduler.createColdObservable([.next(5, "hello"), .completed(10)])
+            let sink = scheduler.createObserver(Bool.self)
+            let disposeBag = DisposeBag()
+
+            let viewModel = UsersViewModel()
+            source.bind(to: viewModel.fetchUsersViewModel(query: "saqib")).disposed(by: disposeBag)
+            viewModel.fetchUsersViewModel(query: "saqib").bind(to: sink).disposed(by: disposeBag)
+
+            scheduler.start()
+
+            XCTAssertEqual(sink.events, [.next(0, true), .next(5, false)])
+        }
 }
